a03f3a02dac07df1378c4b84e1d2714233575bf4
12ryn <zryn1212@gmail.com>
2024-08-21 08:19:31 +0800

    h

---
+++ src/StepTracker.java
@@ -0,0 +1,29 @@
+public class StepTracker {
+
+    private int minSteps;
+
+    public StepTracker(int minSteps){
+
+        this.minSteps = minSteps;
+
+    }
+
+    public int activeDays(){
+
+
+    }
+
+    public double averageSteps(){
+
+
+    }
+
+    public int addDailySteps(){
+
+
+    }
+
+
+
+
+}
